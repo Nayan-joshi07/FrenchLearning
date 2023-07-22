d2574a374dc2956a03e217ad0ffc7301ee0d039d
Nayan Joshi <50129883+Nayan-joshi07@users.noreply.github.com>
2023-07-22 18:11:33 +0530

    init the project

---
+++ app/src/main/java/com/example/frenchteacher/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.frenchteacher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sayTheColor(View view){
+        Button clicked_btn = (Button) view;
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(),
+                getResources().getIdentifier(clicked_btn.getTag().toString(),
+                "raw",
+                getPackageName())
+        );
+
+        mediaPlayer.start();
+
+    }
+}
